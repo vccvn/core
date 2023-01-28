4e5eabd20b3679d2232801851a7528075b28b43d
DoanLN <doanln16@gmail.com>
2023-01-28 22:18:00 +0700

    fix bug

--- src/app/Apis/BaseApi.php
+++ src/app/Apis/BaseApi.php
@@ -164,7 +164,8 @@ abstract class BaseApi
             }
             return $response;
         } catch (BadResponseException $th) {
-            dd($th);
+            dd($th);//
+
             return null;
         }
     }
