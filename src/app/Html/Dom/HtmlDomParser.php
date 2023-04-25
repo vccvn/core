5548a92c132ad8171e960ce089eb736a732e847d
DoanLN <doanln16@gmail.com>
2023-04-25 22:00:34 +0700

    update project

--- src/app/Html/Dom/HtmlDomParser.php
+++ src/app/Html/Dom/HtmlDomParser.php
@@ -11,7 +11,7 @@ require 'simplehtmldom_1_9'.DIRECTORY_SEPARATOR.'simple_html_dom.php';
 class HtmlDomParser {
 	
 	/**
-	 * @return \simplehtmldom_1_5\simple_html_dom
+	 * @return \simplehtmldom_1_9\simple_html_dom
 	 */
 	static public function file_get_html() {
 		return call_user_func_array ( '\simplehtmldom_1_9\file_get_html' , func_get_args() );
@@ -19,7 +19,7 @@ class HtmlDomParser {
 
 	/**
 	 * get html dom from string
-	 * @return \simplehtmldom_1_5\simple_html_dom
+	 * @return \simplehtmldom_1_0\simple_html_dom
 	 */
 	static public function str_get_html() {
 		return call_user_func_array ( '\simplehtmldom_1_9\str_get_html' , func_get_args() );
