24f6be0d86a1d5b8004e70c36fa4d14ecfd97411
DoanLN <doanln16@gmail.com>
2023-12-20 11:59:35 +0700

    update project

--- src/dev/templates/repository.php
+++ src/dev/templates/repository.php
@@ -10,17 +10,17 @@ use App\Validators\FOLDER\NAMEValidator;
 use Illuminate\Http\Request;
 
 /**
- * @method MODELCollection<MODELMask>|MODEL[] filter(Request $request, array $args = []) lấy danh sách MODEL được gán Mask
- * @method MODELCollection<MODELMask>|MODEL[] getFilter(Request $request, array $args = []) lấy danh sách MODEL được gán Mask
- * @method MODELCollection<MODELMask>|MODEL[] getResults(Request $request, array $args = []) lấy danh sách MODEL được gán Mask
- * @method MODELCollection<MODELMask>|MODEL[] getData(array $args = []) lấy danh sách MODEL được gán Mask
- * @method MODELCollection<MODELMask>|MODEL[] get(array $args = []) lấy danh sách MODEL
- * @method MODELCollection<MODELMask>|MODEL[] getBy(string $column, mixed $value) lấy danh sách MODEL
- * @method MODELMask|MODEL getDetail(array $args = []) lấy MODEL được gán Mask
- * @method MODELMask|MODEL detail(array $args = []) lấy MODEL được gán Mask
- * @method MODELMask|MODEL find(integer $id) lấy MODEL
- * @method MODELMask|MODEL findBy(string $column, mixed $value) lấy MODEL
- * @method MODELMask|MODEL first(string $column, mixed $value) lấy MODEL
+ * @method NAMECollection<NAMEMask>|MODEL[] filter(Request $request, array $args = []) lấy danh sách MODEL được gán Mask
+ * @method NAMECollection<NAMEMask>|MODEL[] getFilter(Request $request, array $args = []) lấy danh sách MODEL được gán Mask
+ * @method NAMECollection<NAMEMask>|MODEL[] getResults(Request $request, array $args = []) lấy danh sách MODEL được gán Mask
+ * @method NAMECollection<NAMEMask>|MODEL[] getData(array $args = []) lấy danh sách MODEL được gán Mask
+ * @method NAMECollection<NAMEMask>|MODEL[] get(array $args = []) lấy danh sách MODEL
+ * @method NAMECollection<NAMEMask>|MODEL[] getBy(string $column, mixed $value) lấy danh sách MODEL
+ * @method NAMEMask|MODEL getDetail(array $args = []) lấy MODEL được gán Mask
+ * @method NAMEMask|MODEL detail(array $args = []) lấy MODEL được gán Mask
+ * @method NAMEMask|MODEL find(integer $id) lấy MODEL
+ * @method NAMEMask|MODEL findBy(string $column, mixed $value) lấy MODEL
+ * @method NAMEMask|MODEL first(string $column, mixed $value) lấy MODEL
  * @method MODEL create(array $data = []) Thêm bản ghi
  * @method MODEL update(integer $id, array $data = []) Cập nhật
  */
