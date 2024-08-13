e97e6f72f941210408ca07fde448e7b40f1c119e
DoanLN <doanln16@gmail.com>
2024-08-13 17:33:41 +0700

    update project

--- src/app/Models/Model.php
+++ src/app/Models/Model.php
@@ -85,6 +85,17 @@ class Model extends BaseModel
         return $this->hasOne('App\\Models\\MultiLanguageContent', 'ref_id', 'id')->where('ref', $ref)->where('locale', config('app.locale'));
     }
 
+    /**
+     * Get the localeContents associated with the Model
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function defaultLocaleContent(): HasOne
+    {
+        $ref = defined('static::REF_KEY') ? static::REF_KEY : ($this->table ?? 'data');
+        return $this->hasOne('App\\Models\\MultiLanguageContent', 'ref_id', 'id')->where('ref', $ref)->where('locale', config('app.default_locale', 'default'));
+    }
+
 
     public function rewriteDataIfHasMLC()
     {
@@ -95,6 +106,37 @@ class Model extends BaseModel
             if ($relations && count($relations)) {
                 foreach ($relations as $key => $relation) {
                     if ($relation && $key == 'localeContent') {
+                        $localeContent = $relation;
+                    }
+                }
+            }
+            if(!$localeContent) return;
+            if ($slug = $localeContent->slug) {
+                $this->slug = $slug;
+            }
+            if ($localeContent->title && $this->fillable && in_array('title', $this->fillable))
+                $this->title = $localeContent->title;
+            if ($localeContent->keywords && $this->fillable && in_array('keywords', $this->fillable))
+                $this->keywords = $localeContent->keywords;
+            if (is_array($data = $localeContent->contents)) {
+                foreach ($data as $key => $value) {
+                    if ($value !== null && $value != "")
+                        $this->{$key} = $value;
+                }
+            }
+        }
+    }
+
+
+    public function rewriteDataIfHasDefaultMLC()
+    {
+
+        if ($this->multilang) {
+            $localeContent = null;
+            $relations = $this->getRelations();
+            if ($relations && count($relations)) {
+                foreach ($relations as $key => $relation) {
+                    if ($relation && $key == 'defaultLocaleContent') {
                         $localeContent = $relation;
                     }
                 }
